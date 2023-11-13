f3f4dcb256252a536a7c0d656c6743fb5261ed11
MostafaMemari <144701776+MostafaMemari@users.noreply.github.com>
2023-11-13 17:47:31 +0330

    ✨ feat: create admin model

---
+++ src/api/models/Staff/adminModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, default: "Admin" },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const adminModel = mongoose.model("Admin", adminSchema);
+
+module.exports = {
+  adminModel,
+};
