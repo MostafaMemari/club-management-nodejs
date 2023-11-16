afd5f8002d85f562a3f9fd7cf13b811c92c20717
MostafaMemari <144701776+MostafaMemari@users.noreply.github.com>
2023-11-17 02:10:07 +0330

    ✨ feat: create route get All And get single sport

--- src/api/controllers/club/sportController.js
+++ src/api/controllers/club/sportController.js
@@ -4,6 +4,7 @@ const AsyncHandler = require("express-async-handler");
 const { copyObject, deleteInvalidPropertyInObject } = require("../../helpers/function");
 const { sportModel } = require("../../models/club/sportModel");
 const createError = require("http-errors");
+const { isValidObjectId } = require("mongoose");
 
 //@desc Create Sport
 //@route POST /api/v1/sports
@@ -30,5 +31,34 @@ module.exports.createSport = AsyncHandler(async (req, res) => {
     status: "success",
     message: "رشته ورزشی مورد نظر با موفقیت ثبت شد",
     sportCreated,
+  });
+});
+
+//@desc Get All Sports
+//@route GET /api/v1/sports
+//@acess  Private Admin Only
+module.exports.getSports = AsyncHandler(async (req, res) => {
+  const sportFound = await sportModel.find({}).lean();
+  if (!sportFound) throw createError.InternalServerError("دریافت اطلاعات با خطا مواجه شد");
+  res.status(StatusCodes.OK).json({
+    status: "success",
+    message: "دریافت اطلاعات با موفقیت انجام شد",
+    sportFound,
+  });
+});
+
+//@desc Get Single Sports
+//@route GET /api/v1/sports/:id
+//@acess  Private Admin Only
+module.exports.getSport = AsyncHandler(async (req, res) => {
+  // validate
+  if (!isValidObjectId(req.params.id)) throw createError("شناسه وارد شده اشتباه می باشد");
+
+  const sportFound = await sportModel.findById(req.params.id).lean();
+  if (!sportFound) throw createError.InternalServerError("دریافت اطلاعات با خطا مواجه شد");
+  res.status(StatusCodes.OK).json({
+    status: "success",
+    message: "دریافت اطلاعات با موفقیت انجام شد",
+    sportFound,
   });
 });
--- src/api/routes/club/sportRouter.js.js
+++ src/api/routes/club/sportRouter.js.js
@@ -1,10 +1,12 @@
-const { createSport } = require("../../controllers/club/sportController");
+const { createSport, getSports, getSport } = require("../../controllers/club/sportController");
 const { isAuth } = require("../../middlewares/isAuth");
 const { userModel } = require("../../models/staff/userModel");
 
 const sportRouter = require("express").Router();
 
-sportRouter.route("/").post(isAuth(userModel, "Admin"), createSport);
+sportRouter.route("/").post(isAuth(userModel, "Admin"), createSport).get(getSports);
+
+sportRouter.route("/:id").get(isAuth(userModel, "Admin"), getSport).get(getSports);
 
 module.exports = {
   sportRouter,
