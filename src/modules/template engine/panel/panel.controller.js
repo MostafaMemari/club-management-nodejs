45cc29063fd41dd6029a6e0e34fa819c4a493d73
MostafaMemari <m.memari7@yahoo.com>
2024-03-08 17:44:34 +0330

    ✨ feat: validation register student

--- src/modules/template engine/panel/panel.controller.js
+++ src/modules/template engine/panel/panel.controller.js
@@ -14,8 +14,8 @@ class PanelController {
   async addStudent(req, res, next) {
     try {
       res.locals.layout = "./layouts/panel/add-student.ejs";
-      // res.render("./pages/panel/add-student.ejs");
       res.render("./pages/panel/add-student.ejs");
+      // res.render("./pages/panel/f.ejs");
     } catch (error) {
       next(error);
     }
