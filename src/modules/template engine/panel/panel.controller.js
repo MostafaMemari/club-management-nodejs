e10e3ff6d0019cb953a2eb93e252618c7baef084
MostafaMemari <m.memari7@yahoo.com>
2024-03-21 14:19:47 +0330

    ✨ feat: show list coach | view

--- src/modules/template engine/panel/panel.controller.js
+++ src/modules/template engine/panel/panel.controller.js
@@ -5,18 +5,23 @@ const { UserModel } = require("../../user/user.model");
 const ageGroupService = require("../../baseData/ageGroup/ageGroup.service");
 const beltService = require("../../baseData/belt/belt.service");
 const clubService = require("../../club/club.service");
+const { CoachModel } = require("../../coach/coach.model");
 
 class PanelController {
   #studentService;
   #UserModel;
   #beltService;
   #ageGroupSerivce;
+  #clubService;
+  #coachModel;
   constructor() {
     autoBind(this);
     this.#studentService = studentService;
     this.#UserModel = UserModel;
     this.#beltService = beltService;
     this.#ageGroupSerivce = ageGroupService;
+    this.#clubService = clubService;
+    this.#coachModel = CoachModel;
   }
   async registerStudent(req, res, next) {
     try {
@@ -61,6 +66,28 @@ class PanelController {
       const userAuth = req.userAuth;
       const urlPath = "/coach/register";
       res.render("./pages/panel/coach/register.ejs", { urlPath, userAuth });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async listCoachs(req, res, next) {
+    try {
+      const urlPath = req.path;
+      const userAuth = req.userAuth;
+
+      const clubs = await this.#clubService.getClubsByAdminClubID(userAuth._id);
+      const clubsArr = clubs.map((club) => club._id);
+
+      const coachs = await this.#coachModel
+        .find({ clubs: { $in: clubsArr } })
+        .populate({ path: "clubs", strictPopulate: false, select: "name" })
+        .populate({ path: "belt", strictPopulate: false, select: "name" })
+        .lean();
+
+      const belts = await this.#beltService.find();
+      const ageGroups = await this.#ageGroupSerivce.find();
+
+      res.render("./pages/panel/coach/list.ejs", { coachs, userAuth, belts, ageGroups, urlPath });
     } catch (error) {
       next(error);
     }
--- src/modules/template engine/panel/panel.routes.js
+++ src/modules/template engine/panel/panel.routes.js
@@ -6,10 +6,11 @@ const router = require("express").Router();
 router.get("/", isAuth, PanelController.main);
 
 router.get("/coach/register", isAuth, PanelController.registerCoach);
+router.get("/coachs", isAuth, PanelController.listCoachs);
 
 router.get("/clubs", isAuth, PanelController.listClubs);
 
 router.get("/student/register", isAuth, PanelController.registerStudent);
-router.get("/student/list", isAuth, PanelController.listStudent);
+router.get("/students", isAuth, PanelController.listStudent);
 
 module.exports = { PanelRouter: router };
